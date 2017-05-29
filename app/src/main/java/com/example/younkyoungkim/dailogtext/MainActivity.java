e2c3a5a20d32c818c25d09e97935ba6bf1a28294
kimyounkyoung <passposs2@naver.com>
2017-05-29 19:01:35 +0900

    #4 radio event

--- app/src/main/java/com/example/younkyoungkim/dailogtext/MainActivity.java
+++ app/src/main/java/com/example/younkyoungkim/dailogtext/MainActivity.java
@@ -8,12 +8,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    String[] itemArr={"이경민","김연경","우경원","이정원"};
+    Button butDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button butDialog=(Button) findViewById(R.id.but_dialog);
+        butDialog=(Button) findViewById(R.id.but_dialog);
         butDialog.setOnClickListener(this);
     }
 
@@ -22,8 +23,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         AlertDialog.Builder dialog=new AlertDialog.Builder(this);
         dialog.setTitle("첫번째 다이얼로그");
         dialog.setIcon(R.mipmap.ic_launcher);
-        dialog.setMessage("여기는 메시지를 쓰는 곳입니다.");
-        dialog.setPositiveButton("OK", null);
+        //dialog.setMessage("여기는 메시지를 쓰는 곳입니다.");
+        //단순 목록
+        /*dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                butDialog.setText(itemArr[i]);
+            }
+        });*/
+        //라디오 버튼 목록
+        dialog.setSingleChoiceItems(itemArr, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                butDialog.setText(itemArr[i]);
+            }
+        });
+        //dialog.setPositiveButton("OK", null);
         dialog.show();
     }
 }
+
+
+
+
